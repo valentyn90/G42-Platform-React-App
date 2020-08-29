853a18b8d6330a7384d6366af694b9c1884e118e
masum millat <masum.millat@cramstack.com>
2020-08-30 01:49:41 +0600

    a tag is replaced with Link compo

--- src/pages/analysis/index.js
+++ src/pages/analysis/index.js
@@ -4,52 +4,53 @@ import logo from '../../assets/img/logo.png';
 import realCombatAnalysis from '../../assets/img/realtime-combat-analysis.png';
 import championship from '../../assets/img/championship.png';
 import combatAnalysis from '../../assets/img/combat-analysis.png';
+import {Link} from "react-router-dom";
 const Analysis = () => {
 
     return(
         <div className="container">
             <div className="logo" style={{marginTop: '5%', marginBottom: '30px'}}>
-                <a href="index.php"><img src={logo} /></a>
+                <Link to="/"><img src={logo} alt="logo" /></Link>
             </div>
             <div className="row">
                 <div className="col-md-8">
                     <div className="analysis-item-left">
-                        <a href="#">
+                        <Link to="#">
                             <figure style={{marginBottom: '30px'}}>
                                 <img src={combatAnalysis} alt="profile" />
                                     <figcaption>post - combat analysis</figcaption>
                             </figure>
-                        </a>
+                        </Link>
                         <div className="row">
                             <div className="col-md-6">
-                                <a href="#">
+                                <Link to="#">
                                     <figure>
                                         <img src={championship} alt="profile" style={{height: '320px'}} />
                                             <figcaption>championship</figcaption>
                                     </figure>
-                                </a>
+                                </Link>
                             </div>
                             <div className="col-md-6">
-                                <a href="#">
+                                <Link to="#">
                                     <figure>
                                         <img src={realCombatAnalysis} alt="profile" style={{height: '320px'}} />
                                             <figcaption style={{width: '225px', marginTop: '-37%'}}>real-time combat
                                                 analysis
                                             </figcaption>
                                     </figure>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-4">
                     <div className="analysis-item-right">
-                        <a href="#">
+                        <Link to="#">
                             <figure>
                                 <img src={khabib} alt="profile" />
                                     <figcaption>Fighter Profiling</figcaption>
                             </figure>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import logo from '../../assets/img/logo.png';
+import {Link} from "react-router-dom";
 
 class Home extends React.Component {
   render() {
     return (
       <div className="container-fluid">
         <div className="logo">
-          <a href="/">
-              <img src={logo} />
-          </a>
+          <Link to="/">
+              <img src={logo} alt="logo"/>
+          </Link>
         </div>
         <h3 className="home-title">UFC Data Intelligence Platform</h3>
         <div className="fight-btn-div">
-          <a href="/analysis" className="fight-btn" />
+          <Link to="/analysis" className="fight-btn" />
         </div>
       </div>
     );
