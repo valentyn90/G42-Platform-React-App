50f04d57c3fb6b851db63e29a2979e72094fab3a
masum millat <masum.millat@cramstack.com>
2020-09-02 00:57:32 +0600

    switch box

--- src/pages/comparison/index.js
+++ src/pages/comparison/index.js
@@ -3,11 +3,22 @@ import { useHistory } from 'react-router-dom';
 import httpClient from '../../api/api';
 import ProfileCard from '../../components/ProfileCard';
 import Drawer from '../../components/layouts/drawer';
+import Switch from '../../components/ToggleSwitch';
 
 const Comparison = () => {
   const [fighter, setFighter] = useState(null);
   const [fighter1, setFighter1] = useState(null);
+  const [enabled, setEnable] = useState(false);
+  const [championEnabled, setChampionEnable] = useState(false);
   const history = useHistory();
+
+  const toggleActivityEnabled = (value) => {
+    console.log(value);
+    setEnable(!enabled);
+  };
+  const toggleChampion = (value) =>{
+    setChampionEnable(!championEnabled)
+  }
 
   const params = new URLSearchParams(history.location.search);
   useEffect(() => {
@@ -49,6 +60,29 @@ const Comparison = () => {
           <div className="col-md-2">
             <div className="fight-type">
               <h1 className="name">welter weight</h1>
+              <div className="d-flex switch-box">
+                <Switch
+                  theme=""
+                  className="d-flex"
+                  enabled={enabled}
+                  onStateChanged={() => toggleActivityEnabled()}
+                />
+                <span className="switch-text">
+                  {enabled ? '3 ROUND' : '5 ROUND'}
+                </span>
+              </div>
+              <div className="d-flex switch-box">
+                <Switch
+                  theme=""
+                  className="d-flex"
+                  enabled={championEnabled}
+                  onStateChanged={() => toggleChampion()}
+                />
+                {/* eslint-disable-next-line no-undef */}
+                <span className="switch-text">
+                  {championEnabled ? 'NON CHAMPIONSHIP' : 'CHAMPIONSHIP'}
+                </span>
+              </div>
               <button type="button" className="fight-btn" />
             </div>
           </div>
