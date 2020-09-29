d38363a191fd523f7fad19b89189dd8bde5c23eb
masum millat <masum.millat@cramstack.com>
2020-09-30 02:01:02 +0600

    Updates

---
+++ precache-manifest.d351d8dc83e08a0aa26c94ac90d471e5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd806f6a60998a2de52063c3bdc990b4",
+    "url": "/G42-Platform/index.html"
+  },
+  {
+    "revision": "2ddb8f3a8a96ca366dda",
+    "url": "/G42-Platform/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4c75c5c005aeedf4869d",
+    "url": "/G42-Platform/static/css/main.bec9d516.chunk.css"
+  },
+  {
+    "revision": "2ddb8f3a8a96ca366dda",
+    "url": "/G42-Platform/static/js/2.9a568a51.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/G42-Platform/static/js/2.9a568a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c75c5c005aeedf4869d",
+    "url": "/G42-Platform/static/js/main.dab82c04.chunk.js"
+  },
+  {
+    "revision": "e9f2852f7423da35ad09",
+    "url": "/G42-Platform/static/js/runtime-main.796c2f06.js"
+  },
+  {
+    "revision": "7b683075f39a8c61645382280955e24b",
+    "url": "/G42-Platform/static/media/abraham.7b683075.otf"
+  },
+  {
+    "revision": "045d9f50fcfeb5fb0ecfff6350f70226",
+    "url": "/G42-Platform/static/media/beckman.045d9f50.otf"
+  },
+  {
+    "revision": "2c68d0d1f2a546554d56646db8dcf73a",
+    "url": "/G42-Platform/static/media/championship.2c68d0d1.png"
+  },
+  {
+    "revision": "a568c8f20a9374c3a9605e5be7fdeda1",
+    "url": "/G42-Platform/static/media/combat-analysis.a568c8f2.png"
+  },
+  {
+    "revision": "f027af94370570ec50857e6f269e60e2",
+    "url": "/G42-Platform/static/media/fight-btn.f027af94.png"
+  },
+  {
+    "revision": "b3445bfc1dbe75303a413d5b8c1abc82",
+    "url": "/G42-Platform/static/media/fight-on.b3445bfc.png"
+  },
+  {
+    "revision": "c979eed2c9abf5220c50e3f33b6416fd",
+    "url": "/G42-Platform/static/media/fight.c979eed2.png"
+  },
+  {
+    "revision": "e993ad82c6b31abe688812daa77ff6d5",
+    "url": "/G42-Platform/static/media/khabib.e993ad82.png"
+  },
+  {
+    "revision": "384d3d2b10eb352caaa76aadeace9a39",
+    "url": "/G42-Platform/static/media/logo.384d3d2b.png"
+  },
+  {
+    "revision": "f40534ce25db7f56401b740089551d9d",
+    "url": "/G42-Platform/static/media/menu-toggle.f40534ce.png"
+  },
+  {
+    "revision": "16e6c14ad35c61df26e631798473eecf",
+    "url": "/G42-Platform/static/media/profile-bg.16e6c14a.jpg"
+  },
+  {
+    "revision": "bb5cec6ddfe2f2b3df027da505157bdc",
+    "url": "/G42-Platform/static/media/profiling-bg.bb5cec6d.jpg"
+  },
+  {
+    "revision": "65be53c77f94b2a19f8576dda137f5ca",
+    "url": "/G42-Platform/static/media/realtime-combat-analysis.65be53c7.png"
+  },
+  {
+    "revision": "83b31bcacc5362a72497d8511332874e",
+    "url": "/G42-Platform/static/media/vid.83b31bca.mp4"
+  },
+  {
+    "revision": "7df0b649db28fa6ee51444848f0acbc4",
+    "url": "/G42-Platform/static/media/view-stats.7df0b649.png"
+  },
+  {
+    "revision": "b94ac86f1d4b2278689956e39bc9f802",
+    "url": "/G42-Platform/static/media/wave.b94ac86f.gif"
+  }
+]);
